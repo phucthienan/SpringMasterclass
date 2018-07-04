79a642384b63ed5e0c95789545adda4a2f2ac8ab
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-04 21:40:19 +0700

    Set constructor args using Spring bean config file

--- spring-constructor-args-demo/src/main/java/com/nguyenphucthienan/ConstructorArgsApp.java
+++ spring-constructor-args-demo/src/main/java/com/nguyenphucthienan/ConstructorArgsApp.java
@@ -3,16 +3,19 @@ package com.nguyenphucthienan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class IOCApp {
+public class ConstructorArgsApp {
     public static void main(String[] args) {
         // Create the application context (container)
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
         // Create the bean
-        Organization organization = (Organization) context.getBean("organization");
+        Organization organization = (Organization) context.getBean("secondOrganization");
 
         // Invoke the company slogan via the bean
         organization.corporateSlogan();
+
+        // Print organization details
+        System.out.println(organization);
 
         // Close the application context (container)
         ((ClassPathXmlApplicationContext) context).close();
--- spring-constructor-args-demo/src/main/java/com/nguyenphucthienan/Organization.java
+++ spring-constructor-args-demo/src/main/java/com/nguyenphucthienan/Organization.java
@@ -1,8 +1,27 @@
 package com.nguyenphucthienan;
 
 public class Organization {
+    private String companyName;
+    private int yearOfIncorporation;
+
+    // public Organization() {
+    // }
+
+    public Organization(String companyName, int yearOfIncorporation) {
+        this.companyName = companyName;
+        this.yearOfIncorporation = yearOfIncorporation;
+    }
+
     public void corporateSlogan() {
         String slogan = "My Slogan";
         System.out.println(slogan);
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "companyName='" + companyName + '\'' +
+                ", yearOfIncorporation=" + yearOfIncorporation +
+                '}';
     }
 }
