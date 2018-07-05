2485239b45b87c1a7e222656e6c3c16869172c5e
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-05 19:46:13 +0700

    Init spring-pnamespace-demo project

---
+++ spring-pnamespace-demo/src/main/java/com/nguyenphucthienan/PNamespaceApp.java
@@ -0,0 +1,21 @@
+package com.nguyenphucthienan;
+
+import com.nguyenphucthienan.domain.Organization;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PNamespaceApp {
+    public static void main(String[] args) {
+        // Create the application context (container)
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // Create the bean
+        Organization organization = (Organization) context.getBean("organization");
+
+        // Print organization details
+        System.out.println(organization);
+
+        // Close the application context (container)
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
---
+++ spring-pnamespace-demo/src/main/java/com/nguyenphucthienan/domain/Organization.java
@@ -0,0 +1,22 @@
+package com.nguyenphucthienan.domain;
+
+public class Organization {
+    private String companyName;
+    private int yearOfIncorporation;
+    private Address address;
+
+    public Organization(String companyName, int yearOfIncorporation, Address address) {
+        this.companyName = companyName;
+        this.yearOfIncorporation = yearOfIncorporation;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "companyName='" + companyName + '\'' +
+                ", yearOfIncorporation=" + yearOfIncorporation +
+                ", address=" + address +
+                '}';
+    }
+}
