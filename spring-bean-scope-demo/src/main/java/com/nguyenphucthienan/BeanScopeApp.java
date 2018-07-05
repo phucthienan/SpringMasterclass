0b3595e5f8395706c6560ae183e909cc2a2601f2
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-05 15:23:57 +0700

    Init spring-bean-scope-demo project

---
+++ spring-bean-scope-demo/src/main/java/com/nguyenphucthienan/BeanScopeApp.java
@@ -0,0 +1,25 @@
+package com.nguyenphucthienan;
+
+import com.nguyenphucthienan.domain.Organization;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeApp {
+    public static void main(String[] args) {
+        // Create the application context (container)
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // Create the bean
+        Organization organization = (Organization) context.getBean("organization");
+
+        // Invoke the company slogan via the bean
+        System.out.println(organization.corporateSlogan());;
+
+        // Print organization details
+        System.out.println(organization);
+        System.out.println(organization.corporateService());
+
+        // Close the application context (container)
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
---
+++ spring-bean-scope-demo/src/main/java/com/nguyenphucthienan/service/BusinessService.java
@@ -0,0 +1,7 @@
+package com.nguyenphucthienan.service;
+
+public interface BusinessService {
+    public int revenue = 1000000;
+
+    public String offerService(String companyName);
+}
