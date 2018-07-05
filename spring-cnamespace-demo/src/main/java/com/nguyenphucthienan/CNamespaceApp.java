765ae8c2373cf750086a8efbc29318b870f1857b
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-05 19:20:55 +0700

    Init spring-cnamespace-demo project

---
+++ spring-cnamespace-demo/src/main/java/com/nguyenphucthienan/CNamespaceApp.java
@@ -0,0 +1,24 @@
+package com.nguyenphucthienan;
+
+import com.nguyenphucthienan.domain.Organization;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CNamespaceApp {
+    public static void main(String[] args) {
+        // Create the application context (container)
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // Create the bean
+        Organization organization = (Organization) context.getBean("organization");
+
+        // Invoke the company slogan via the bean
+        organization.corporateSlogan();
+
+        // Print organization details
+        System.out.println(organization);
+
+        // Close the application context (container)
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
