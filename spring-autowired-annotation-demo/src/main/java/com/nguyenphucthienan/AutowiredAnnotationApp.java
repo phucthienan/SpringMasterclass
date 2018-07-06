62adc8300b3db2cf34a9ae60b529d2b702ba35cc
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-06 19:15:55 +0700

    Autowiring Spring beans using setters, contructors and fields

--- spring-autowired-annotation-demo/src/main/java/com/nguyenphucthienan/AutowiredAnnotationApp.java
+++ spring-autowired-annotation-demo/src/main/java/com/nguyenphucthienan/AutowiredAnnotationApp.java
@@ -1,5 +1,6 @@
 package com.nguyenphucthienan;
 
+import com.nguyenphucthienan.domain.promotion.TradeFair;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +8,9 @@ public class AutowiredAnnotationApp {
     public static void main(String[] args) {
         // Create the application context (container)
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        TradeFair tradeFair = (TradeFair) context.getBean("tradeFair");
+        System.out.println(tradeFair.specialPromotionPricing());
 
         // Close the application context (container)
         ((ClassPathXmlApplicationContext) context).close();
--- spring-autowired-annotation-demo/src/main/java/com/nguyenphucthienan/domain/promotion/TradeFair.java
+++ spring-autowired-annotation-demo/src/main/java/com/nguyenphucthienan/domain/promotion/TradeFair.java
@@ -2,10 +2,33 @@ package com.nguyenphucthienan.domain.promotion;
 
 import com.nguyenphucthienan.domain.product.ConsumerProduct;
 import com.nguyenphucthienan.domain.product.IndustrialProduct;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TradeFair {
+    // @Autowired
     private IndustrialProduct industrialProduct;
+
+    // @Autowired
     private ConsumerProduct consumerProduct;
+
+    public TradeFair() {
+    }
+
+    @Autowired
+    public TradeFair(IndustrialProduct industrialProduct, ConsumerProduct consumerProduct) {
+        this.industrialProduct = industrialProduct;
+        this.consumerProduct = consumerProduct;
+    }
+
+    // @Autowired
+    // public void setIndustrialProduct(IndustrialProduct industrialProduct) {
+    //     this.industrialProduct = industrialProduct;
+    // }
+    //
+    // @Autowired
+    // public void setConsumerProduct(ConsumerProduct consumerProduct) {
+    //     this.consumerProduct = consumerProduct;
+    // }
 
     public int declareIndustrialProductPrice(IndustrialProduct industrialProduct) {
         return industrialProduct.calculatePrice();
